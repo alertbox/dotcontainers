0148ae8d0686c0415d7f5b3602c2169a4f78387e
kosperera <therealkosala@gmail.com>
2025-05-25 00:43:17 -0400

    refactor: improve code readability and maintainability

--- src/opentofu/install.sh
+++ src/opentofu/install.sh
@@ -1,50 +1,55 @@
 #!/usr/bin/env bash
 
-set -e
-
-OPENTOFU_VERSION=${VERSION:-"latest"}
-
-echo "Activating feature 'opentofu@${OPENTOFU_VERSION}'"
-
 # The 'install.sh' entrypoint script is always executed as the root user.
-apt_get_update() {
-    echo "Running apt-get update..."
-    apt-get update -y
-}
+#
+# This script installs OpenTofu.
+# Source: https://bun.sh/install
+#         https://github.com/oven-sh/bun/tree/main/dockerhub
+
+set -e
 
 # Checks if packages are installed and installs them if not
 check_packages() {
     if ! dpkg -s "$@" > /dev/null 2>&1; then
         if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-            apt_get_update
+            echo "Running apt-get update..."
+            apt-get update -qq
         fi
-        apt-get -y install --no-install-recommends "$@"
+        apt-get -qq install --no-install-recommends "$@"
+        apt-get clean
     fi
 }
 
 export DEBIAN_FRONTEND=noninteractive
 
-check_packages curl
+# https://github.com/opentofu/opentofu/releases
+VERSION=${VERSION:-"latest"}
 
-install() {
-    # Download the installer script:
-    curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
-    # Alternatively: wget --secure-protocol=TLSv1_2 --https-only https://get.opentofu.org/install-opentofu.sh -O install-opentofu.sh
+echo "Activating feature 'tofu'"
 
-    # Give it execution permissions:
-    chmod +x install-opentofu.sh
+# Clean up.
+rm -rf /var/lib/apt/lists/*
 
-    # Please inspect the downloaded script
+check_packages ca-certificates curl dirmngr gpg gpg-agent
 
-    # Run the installer:
-    ./install-opentofu.sh --install-method deb --opentofu-version ${OPENTOFU_VERSION}
-}
+case "${VERSION}" in
+    latest | v*) TAG="$VERSION"; ;;
+    *)           TAG="v${VERSION}"; ;;
+esac
 
-echo "(*) Installing OpenTofu (Tofu)..."
+INSTALLER="install-opentofu.sh"
 
-install
+# Download the installer script:
+curl "https://get.opentofu.org/${INSTALLER}" --proto '=https' --tlsv1.2 -fsSLO --compressed --retry 5 ||
+      (echo "error: failed to download: ${TAG}" && exit 1)
+
+# Give it execution permissions:
+chmod +x $INSTALLER
+
+su ${_REMOTE_USER} -c "./${INSTALLER} --install-method deb --opentofu-version ${VERSION}" ||
+    (echo "error: failed to install tofu." && exit 1)
 
 # Clean up
-rm -f install-opentofu.sh
+rm -rf $INSTALLER
 
 echo "Done!"
